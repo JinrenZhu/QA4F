f01e98701a7feb4bc0f1fddbc01fba36bce2686c
Jin-ren Zhu <jinren.zhu@outlook.com>
2023-06-18 06:43:25 +0000

    A 101 script to test the workflow on QPU

---
+++ test00_BooleanGate.py
@@ -0,0 +1,12 @@
+# %% prelim
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+# %% QUBO
+Q = {('x', 'x'): -1, ('x', 'z'): 2, ('z', 'x'): 0, ('z', 'z'): -1}
+
+# %% quantum sampler
+sampler = EmbeddingComposite(DWaveSampler())
+sampleset = sampler.sample_qubo(Q, num_reads=5000, label='SDK Examples - NOT Gate')
+
+# %% output
+print(sampleset)

46fb9d8c568c8b67a34d132a9269e8fa9a16819a
Jin-ren Zhu <bpe@qq.com>
2023-07-20 02:41:21 +0000

    adjust chain strength in channel problem

--- test03/QA_bin.py
+++ test03/QA_bin.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from dimod import BinaryQuadraticModel
 from dwave.system import DWaveSampler, EmbeddingComposite
+from dwave.inspector import show
 
 # %% parameters
 # 12 real varialbes * 10 binary encoding = 120 variables
@@ -16,12 +17,14 @@ range_encode = 20
 nel = 6
 nen = nel * 2 + 1
 # Sampler
-QA_n_reads = 4000
+QA_n_reads = 2000
 QA_sample_rtol = 1.0e-4
+QA_chain_strength = 1000
 # suffix
 sffx_1 = str(n_real).zfill(2)
 sffx_2 = str(n_encode).zfill(2)
 sffx_3 = str(QA_n_reads).zfill(5)
+sffx_4 = str(QA_chain_strength).zfill(4)
 
 # %% load QUBO coefficient
 cov_matrix_off = np.load('cov_mat_off_' + sffx_1 + '_' + sffx_2 + '.npy')
@@ -34,12 +37,16 @@ bqm_channel.add_linear_from_array(bias_linear)
 bqm_channel.add_quadratic_from_dense(cov_matrix_off)
 
 samplerQA = EmbeddingComposite(DWaveSampler())
-sampleset_QA = samplerQA.sample(bqm_channel, num_reads=QA_n_reads, label='QFI-Channel-6bits')
+sampleset_QA = samplerQA.sample(bqm_channel,
+                                num_reads=QA_n_reads,
+                                chain_strength=QA_chain_strength,
+                                label='QFI-Channel-4bits')
+
+show(sampleset_QA) 
 
 print(sampleset_QA.first)
 print(len(sampleset_QA.lowest(rtol=QA_sample_rtol))/len(sampleset_QA))
 
-np.save('QA_samples_' + sffx_1 + '_' + sffx_2 + '_' + sffx_3 + '.npy',
-        sampleset_QA.record)
-np.save('QA_samples_lowest_' + sffx_1 + '_' + sffx_2 + '_' + sffx_3 + '.npy',
-        sampleset_QA.lowest(rtol=QA_sample_rtol).record)
+sffx_all = sffx_1 + '_' + sffx_2 + '_' + sffx_3 + '_' + sffx_4 + '.npy'
+np.save('QA_samples_' + sffx_all, sampleset_QA.record)
+np.save('QA_samples_lowest_' + sffx_all, sampleset_QA.lowest(rtol=QA_sample_rtol).record)
